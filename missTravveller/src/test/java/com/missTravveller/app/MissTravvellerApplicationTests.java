cb084c75ed152c31c38221a11cb81daaef976e63
Diana Gallegos <diana_prz21@hotmail.com>
2024-08-02 22:56:50 -0600

    Cree mi rama Itinerary

---
+++ missTravveller/src/test/java/com/missTravveller/app/MissTravvellerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.missTravveller.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissTravvellerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
