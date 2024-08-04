1de72ef39214a11e603e25c8d1531cb6a29589cb
Diana Gallegos <diana_prz21@hotmail.com>
2024-08-04 09:28:59 -0600

    Finished: clase e interfaz de Itinerary

--- missTravveller/src/main/java/com/missTravveller/app/model/Itinerary.java
+++ missTravveller/src/main/java/com/missTravveller/app/model/Itinerary.java
@@ -1,7 +1,7 @@
 package com.missTravveller.app.model;
 
 
-
+import java.sql.Timestamp;
 
 import jakarta.persistence.*;
 
@@ -15,5 +15,91 @@ public class Itinerary {
 	private String activity;
 	private Timestamp start_activity;
 	private Timestamp end_activity;
+	private String meeting_point;
+	private String travel;
+	
+	public Itinerary() {
+		
+	}
+
+	public Itinerary(Long id, String activity, Timestamp start_activity, Timestamp end_activity, String meeting_point,
+			String travel) {
+		super();
+		this.id = id;
+		this.activity = activity;
+		this.start_activity = start_activity;
+		this.end_activity = end_activity;
+		this.meeting_point = meeting_point;
+		this.travel = travel;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getActivity() {
+		return activity;
+	}
+
+	public void setActivity(String activity) {
+		this.activity = activity;
+	}
+
+	public Timestamp getStart_activity() {
+		return start_activity;
+	}
+
+	public void setStart_activity(Timestamp start_activity) {
+		this.start_activity = start_activity;
+	}
+
+	public Timestamp getEnd_activity() {
+		return end_activity;
+	}
+
+	public void setEnd_activity(Timestamp end_activity) {
+		this.end_activity = end_activity;
+	}
+
+	public String getMeeting_point() {
+		return meeting_point;
+	}
+
+	public void setMeeting_point(String meeting_point) {
+		this.meeting_point = meeting_point;
+	}
+
+	public String getTravel() {
+		return travel;
+	}
+
+	public void setTravel(String travel) {
+		this.travel = travel;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Itinerary [id=");
+		builder.append(id);
+		builder.append(", activity=");
+		builder.append(activity);
+		builder.append(", start_activity=");
+		builder.append(start_activity);
+		builder.append(", end_activity=");
+		builder.append(end_activity);
+		builder.append(", meeting_point=");
+		builder.append(meeting_point);
+		builder.append(", travel=");
+		builder.append(travel);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	
 	
 }
--- missTravveller/src/main/java/com/missTravveller/app/repository/ItineraryRepository.java
+++ missTravveller/src/main/java/com/missTravveller/app/repository/ItineraryRepository.java
@@ -1,5 +1,22 @@
 package com.missTravveller.app.repository;
 
-public interface ItineraryRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.sql.Timestamp;
+import java.util.*;
+
+import com.missTravveller.app.model.Itinerary;
+
+@RepositoryRestResource(collectionResourceRel = "itineraries", path = "itineraries")
+public interface ItineraryRepository extends CrudRepository<Itinerary, Long>, PagingAndSortingRepository<Itinerary, Long> {
+	
+	List<Itinerary> findByActivity(String activity);
+    List<Itinerary> findByMeetingPoint(String meeting_point);
+    List<Itinerary> findByTravel(String travel);
+    List<Itinerary> findByStartActivity(Timestamp start_activity);
+    List<Itinerary> findByEndActivity(Timestamp end_activity);
+    boolean existsByActivity(String activity);
+    
 }
