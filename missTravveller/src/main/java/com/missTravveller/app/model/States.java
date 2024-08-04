92633d7ade70117f8322659ab61c311f438233d0
Abigail Ramirez <abigailrg03@gmail.com>
2024-08-03 19:45:02 -0600

    add: añado contenido en clase States

--- missTravveller/src/main/java/com/missTravveller/app/model/States.java
+++ missTravveller/src/main/java/com/missTravveller/app/model/States.java
@@ -1,5 +1,64 @@
 package com.missTravveller.app.model;
 
+import java.util.UUID;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="states")
 public class States {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private UUID id;
+	
+	@Column(name = "state_name", length = 100, nullable =false)
+	private String nombre;
+	
+	//constructor
+	public States() {
+		
+	}
+	
+	//constructor using fields
+	public States(UUID id, String nombre) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+	}
+	
+	//setters and getters
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	//String
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("States [id=");
+		builder.append(id);
+		builder.append(", nombre=");
+		builder.append(nombre);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	//Relacion con Tabla Travels
+	
+	
+	
 
 }
