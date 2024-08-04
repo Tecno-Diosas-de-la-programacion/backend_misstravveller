a6efdaee2667ec59ecc3009766d7aba3ea9c3642
Karina Robles <roblesks92@gmail.com>
2024-08-04 17:19:12 -0600

    Creación de interfaz TravelsRepository.java

--- missTravveller/src/main/java/com/missTravveller/app/repository/TravelsRepository.java
+++ missTravveller/src/main/java/com/missTravveller/app/repository/TravelsRepository.java
@@ -1,5 +1,21 @@
 package com.missTravveller.app.repository;
 
-public interface TravelsRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.util.Optional;
+import java.util.UUID;
+
+import com.missTravveller.app.model.Travels;
+
+public interface TravelsRepository extends CrudRepository<Travels, UUID>, PagingAndSortingRepository<Travels, UUID> {
+
+	//revisar 
+	
+	Optional<Travels> findByEmail(String email);	
+	Iterable<Travels> findAllByActiveTrue();
+	Iterable<Travels> findAllByActiveFalse();
+	boolean existsByEmail(String email);
+	
 }
+
