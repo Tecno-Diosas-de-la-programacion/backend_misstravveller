d9896859679bab053d3a0d2512036d7054f90dd2
Arantxa Vazquez <arantxavazquez177@gmail.com>
2024-08-05 16:16:13 -0600

    change: controladorUser

--- missTravveller/src/main/java/com/missTravveller/app/controller/UserDTOController.java
+++ missTravveller/src/main/java/com/missTravveller/app/controller/UserDTOController.java
@@ -1,12 +1,65 @@
 package com.missTravveller.app.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.UUID;
 
-@RestController
-@RequestMapping("api/v2/users")
-@CrossOrigin(origins = "*")
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import com.missTravveller.app.dto.UserDTO;
+import com.missTravveller.app.model.User;
+import com.missTravveller.app.service.UserDTOService;
+
+@RestController //indica que es controller de API REST
+@RequestMapping("api/v1/users") // endpoints
+@CrossOrigin(origins = "*") //Permite origenes cruzados, de los puertos. 
 public class UserDTOController {
 
+	//llamamos a nuestro servicio 
+	UserDTOService userDTOService;
+	
+	//metodo contructor para generar Bean 
+	//sources>generate contructor by fiels
+
+	public UserDTOController(UserDTOService userDTOService) {
+		this.userDTOService = userDTOService;
+	}
+	
+
+	//maping usando todos los metodos de service CRUD
+	// return un estatus especifico
+	
+	
+	//crear usuario y devolver 202
+	@PostMapping
+	ResponseEntity<UserDTO> createUser(@RequestBody User user) {
+		UserDTO newUser = userDTOService.createUser(user);
+		return ResponseEntity.status(201).body(newUser);
+	}
+
+	//buscar usuario por Id
+	
+	@GetMapping("{id}")
+	ResponseEntity<UserDTO> getUserById(@PathVariable UUID id) {
+		UserDTO existingUser = userDTOService.getUserById(id);
+		return ResponseEntity.ok(existingUser);
+	}
+
+	//
+	
+	@PutMapping("{id}")
+	ResponseEntity<UserDTO> updateUser(@RequestBody User newUserData, @PathVariable UUID id) {
+		UserDTO updatedUser = userDTOService.updateUser(newUserData, id);
+		return ResponseEntity.ok(updatedUser);
+	}
+	
+	@DeleteMapping("{id}")
+	ResponseEntity<String> deleteUser(@PathVariable UUID id) {
+		userDTOService.deleteUser(id);
+		return ResponseEntity.status(204).body("User id " + id + " successfully deleted");
+	}
+
+	
+	
+	
 }
