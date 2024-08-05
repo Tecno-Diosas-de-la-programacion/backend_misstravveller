40ad64609e09df163aab8f27f9fbe09d5dcb1ff9
Arantxa Vazquez <arantxavazquez177@gmail.com>
2024-08-04 19:53:35 -0600

    creacion modelo y respositorio user y rol

--- missTravveller/src/main/java/com/missTravveller/app/model/Rol.java
+++ missTravveller/src/main/java/com/missTravveller/app/model/Rol.java
@@ -1,5 +1,59 @@
 package com.missTravveller.app.model;
 
-public class Rol {
+import jakarta.persistence.*;
 
+
+@Entity
+@Table(name="roles")
+public class Rol {
+	
+	@Id 
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name="name", length=45, nullable=false)
+	private String name;
+	
+
+	//metodo vacio
+	public Rol() {}
+	
+	
+
+	public Rol(Long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public static Rol setUserRole(Long roleId) {
+		return new Rol(  (Long) roleId, "");
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	
 }
--- missTravveller/src/main/java/com/missTravveller/app/repository/RolRepository.java
+++ missTravveller/src/main/java/com/missTravveller/app/repository/RolRepository.java
@@ -1,5 +1,11 @@
 package com.missTravveller.app.repository;
 
-public interface RolRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.missTravveller.app.model.Rol;
+
+@Repository
+public interface RolRepository extends CrudRepository<Rol, Long> {
 
 }
