cef183e095f3dcbf48fdba2ab7ea8d7af2274b03
Abigail Ramirez <abigailrg03@gmail.com>
2024-08-04 21:52:37 -0600

    Se corrige en intergaz de UUID a Long

--- missTravveller/src/main/java/com/missTravveller/app/repository/StatesRespository.java
+++ missTravveller/src/main/java/com/missTravveller/app/repository/StatesRespository.java
@@ -1,14 +1,13 @@
 package com.missTravveller.app.repository;
 
 import java.util.*;
-import java.util.UUID;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import com.missTravveller.app.model.States;
 
 //revisar
-public interface StatesRespository extends CrudRepository<States, UUID>, PagingAndSortingRepository<States, UUID> {
+public interface StatesRespository extends CrudRepository<States, Long>, PagingAndSortingRepository<States, Long> {
 	
 	List<States> findAllByStateName(@Param("value") String stateName);
 
