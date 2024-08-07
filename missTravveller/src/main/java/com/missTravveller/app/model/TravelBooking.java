f3f2bab9cbfc31010a175c69fb4b56bfb091e0f6
Karina Robles <roblesks92@gmail.com>
2024-08-07 00:11:46 -0600

    agrego trabajo de Dulce en su clase e interfaz travels booking y mejoras a mi clase e interfaz travels

--- missTravveller/src/main/java/com/missTravveller/app/model/TravelBooking.java
+++ missTravveller/src/main/java/com/missTravveller/app/model/TravelBooking.java
@@ -1,5 +1,46 @@
 package com.missTravveller.app.model;
 
-public class TravelBooking {
+import java.util.UUID;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name="travelBooking")
+public class TravelBooking { 
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private UUID id;
+	
+	public TravelBooking() {
+		
+	}
+
+	public TravelBooking(UUID id) {
+		super();
+		this.id = id;
+	}
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("TravelBooking [id=");
+		builder.append(id);
+		builder.append("]");
+		return builder.toString();
+	} 
+	
+	
 }
+// TRABAJAR LA RELACION ENTRE TABLAS 
--- missTravveller/src/main/java/com/missTravveller/app/repository/TravelsRepository.java
+++ missTravveller/src/main/java/com/missTravveller/app/repository/TravelsRepository.java
@@ -1,5 +1,25 @@
 package com.missTravveller.app.repository;
 
-public interface TravelsRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.math.BigInteger;
+import java.sql.Timestamp;
+import java.util.*;
+
+import com.missTravveller.app.model.Travel;
+
+@RepositoryRestResource(collectionResourceRel = "travels", path = "travels")
+public interface TravelsRepository extends CrudRepository<Travel, UUID>, PagingAndSortingRepository<Travel,  UUID> {
+	
+	List<Travel> findBydestination(String destination);
+    List<Travel> findByprice(Double price);
+    List<Travel> findBydescription(String description);
+    List<Travel> findByimage(Timestamp image);
+    List<Travel> findBytraveldate(Timestamp traveldate);
+    List<Travel> findByquota(BigInteger quota);
+    //List<Travel> findBystatesid(int statesid);
+   
+    
 }
