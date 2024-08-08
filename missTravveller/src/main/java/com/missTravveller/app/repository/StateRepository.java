967f7170310c7534efaa7d0abadbf7d8c4a93499
Abigail Ramirez <abigailrg03@gmail.com>
2024-08-07 23:03:49 -0600

    update: agrego service implementation y update de states

--- missTravveller/src/main/java/com/missTravveller/app/repository/StateRepository.java
+++ missTravveller/src/main/java/com/missTravveller/app/repository/StateRepository.java
@@ -4,11 +4,15 @@ import java.util.*;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
 import com.missTravveller.app.model.State;
 
 //revisar
-public interface StatesRepository extends CrudRepository<State, Long>, PagingAndSortingRepository<State, Long> {
+@RepositoryRestResource(collectionResourceRel = "states", path = "states")
+public interface StateRepository extends CrudRepository<State, Long>, PagingAndSortingRepository<State, Long> {
 	
 	List<State> findAllByStateName(@Param("value") String stateName);
-
+	boolean existsByName(String name);
+	Optional<State>findByName(String name);
 }
--- missTravveller/src/main/java/com/missTravveller/app/service/impl/StateServiceImpl.java
+++ missTravveller/src/main/java/com/missTravveller/app/service/impl/StateServiceImpl.java
@@ -1,8 +1,59 @@
 package com.missTravveller.app.service.impl;
+import com.missTravveller.app.model.State;
+import com.missTravveller.app.repository.StateRepository;
+import com.missTravveller.app.service.StateService;
+import com.missTravveller.app.util.StateUpdate;
+
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 @Service
-public class StateServiceImpl {
+public class StateServiceImpl implements StateService {
+	
+	StateRepository stateRepository;
+	StateService stateService;
+	
+	public StateServiceImpl(StateRepository stateRepository, StateService stateService) {
+		this.stateRepository = stateRepository;
+		this.stateService = stateService;
+	}
+
+	@Override
+	public State createStates(State state) {
+		String name = state.getName();
+		if( stateRepository.existsByName(name) ) {
+			throw new IllegalStateException("The state " + name + " is already registered.");
+		}
+		state.setId(null);
+		State newState = saveState(state);
+		return newState;
+	}
+
+	private State saveState(State state) {
+		return stateRepository.save(state);
+	}
+
+	@Override
+	public State getStatesById(Long id) {
+		Optional<State> optionalState = stateRepository.findById(id);
+		if( optionalState.isEmpty() ) {
+			throw new IllegalStateException("State does not exist with id " + id);
+		}
+		return optionalState.get();
+	}
+
+	@Override
+	public State updateStates(State state, Long id) {
+		State existingState = getStatesById(id);
+		return StateUpdate.updateState(existingState, state);
+	}
+
+	@Override
+	public void deleteStates(Long id) {
+		State existingState = getStatesById(id);
+		saveState(existingState);
+		
+	}
 
 }
